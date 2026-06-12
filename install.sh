af4c628a5f07af1c8adcbce92ba2f9050bbcd21b
qq979338111 <66366234+qq979338111@users.noreply.github.com>
2026-06-12 20:01:30 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
-# 清除代理避免干扰
 unset http_proxy https_proxy HTTP_PROXY HTTPS_PROXY
 rm -f SAOML5
-# GitHub原生直链，路径100%准确
 wget --no-proxy -O SAOML5 https://raw.githubusercontent.com/QQ979338111/SAOML2026/main/SAOML5
 chmod +x SAOML5
 sleep 0.3
