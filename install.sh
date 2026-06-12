9685279fa289739a5c30f7644e8e1a9dcd34c9a7
qq979338111 <66366234+qq979338111@users.noreply.github.com>
2026-06-12 20:11:00 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
+# 清空代理
 unset http_proxy https_proxy HTTP_PROXY HTTPS_PROXY
+# 删除旧文件，注意这里文件名大写SAOML5
 rm -f SAOML5
-wget --no-proxy -O SAOML5 https://raw.githubusercontent.com/QQ979338111/SAOML2026/main/SAOML5
+# ghproxy国内加速链接，文件名大写SAOML5，分支main
+wget --no-proxy -O SAOML5 https://ghproxy.com/https://raw.githubusercontent.com/QQ979338111/SAOML2026/main/SAOML5
 chmod +x SAOML5
 sleep 0.3
+# 运行大写文件名程序
 ./SAOML5
