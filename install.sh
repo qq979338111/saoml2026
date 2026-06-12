aecfacd13552ee4787973effc8f5a738ee528363
qq979338111 <66366234+qq979338111@users.noreply.github.com>
2026-06-12 19:56:28 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
+# 清除代理防止下载卡住
 unset http_proxy https_proxy HTTP_PROXY HTTPS_PROXY
-rm -rf saoml5
-wget --no-proxy -O saoml5 https://cdn.jsdelivr.net/gh{qq979338111}/{SAOML2026
-}@main/saoml5
-chmod +x saoml5
+# 删除旧文件
+rm -f SAOML5
+# 从你的GitHub CDN拉取程序
+wget --no-proxy -O SAOML5 https://cdn.jsdelivr.net/gh/QQ979338111/SAOML2026@main/SAOML5
+# 赋予执行权限
+chmod +x SAOML5
+# 延迟解决Text file busy报错
 sleep 0.3
-./saoml5
+# 运行程序
+./SAOML5
