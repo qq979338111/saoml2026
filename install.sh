e52bfbe67d046a94f2cc6b7aa372cc35fed4a445
qq979338111 <66366234+qq979338111@users.noreply.github.com>
2026-06-12 20:46:08 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@
 unset http_proxy https_proxy HTTP_PROXY HTTPS_PROXY
 rm -f SAOML5
 # 更换稳定代理，标准github raw路径格式，无跳转
-wget --no-proxy -O SAOML5 https://mirror.ghproxy.com/https://github.com/QQ979338111/SAOML2026/raw/main/SAOML5
+wget --no-proxy -O SAOML5 https://gh.ddlc.top/https://github.com/QQ979338111/SAOML2026/raw/main/SAOML5
 chmod +x SAOML5
 sleep 0.3
 ./SAOML5
